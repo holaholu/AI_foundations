7241d9fb913832e512f7bc0a088f31fbd5f336cc
holaholu <olaoluhimself@yahoo.com>
2025-09-01 14:16:54 -0500

    Week 1  edit

--- Code_files/Week11/Day1/dat1_tensorflow.py
+++ Code_files/Week11/Day1/dat1_tensorflow.py
@@ -5,7 +5,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding, SimpleRNN, Dense
 
 vocab_size = 10000
-max_len = 200
+max_len = 200 # hyperparameter 1
 
 (X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=vocab_size)
 
