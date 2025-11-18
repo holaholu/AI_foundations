4dfb133bc83b47def889002c730018948a4fe5ba
holaholu <olaoluhimself@yahoo.com>
2025-11-18 06:07:37 -0600

    test1

--- Projects/Misc_Projects/Hugglang/hugglang.py
+++ Projects/Misc_Projects/Hugglang/hugglang.py
@@ -24,7 +24,7 @@ llm = HuggingFacePipeline(pipeline=model)  # Use 'pipeline' parameter
 prompt_template = PromptTemplate.from_template(
     "Explain {topic} in detail for a {age} year old to understand")
 
-# Create the chains proper\
+# Create the chains properLY
 chain = prompt_template | llm
 
 topic = input("Topic: ")
